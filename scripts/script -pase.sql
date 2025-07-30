f2ee18b8b03765cb2eb8d43385c122b6bf97da0b
cristian <cristian2.10@hotmail.com>
2025-07-30 16:51:35 -0500

    add: se agrego el campo puesto de trabajo en evluacion oftalmologica

--- scripts/script -pase.sql
+++ scripts/script -pase.sql
@@ -1,15 +1,7 @@
-DROP FUNCTION obtener_reporte_evaluacionoftalmologica(integer);
+drop FUNCTION obtener_reporte_evaluacionoftalmologica(IN p_norden integer)
 
 CREATE OR REPLACE FUNCTION obtener_reporte_evaluacionoftalmologica(IN p_norden integer)
-  RETURNS TABLE(nombre text, apellido text, edad text, n_orden integer, dni integer, fecha_nac date, empresa text, contrata text, nom_examen text, sexo "char", cod_of integer, fecha_of date, rbecptosisod boolean, rbecptosisoi boolean, rbecestrabismood boolean, rbecestrabismooi boolean, rbecconjuntivitisod boolean, rbecconjuntivitisoi boolean, rbeccataratasod boolean, rbeccataratasoi boolean, rbecpterigionod boolean, rbecpterigionoi boolean, rbecpingueculaod boolean, rbecpingueculaoi boolean, rbecclalacionod boolean, rbecclalacionoi boolean, rbecotrosod boolean, rbecotrosoi boolean, txtechallazgos text, rbfonormalod boolean, rbfonormaloi boolean, rbfoanormalod boolean, rbfoanormaloi boolean, txtfohallazgos text, txtpiood text, txtpiooi text, txtpiona text, rbco_si boolean, rbco_no boolean, rbco_cerca boolean, rbco_lejos boolean, chkntcc boolean, chkntcl boolean, txtcercasincorregirod text, txtcercasincorregiroi text, txtlejossincorregirod text, txtlejossincorregiroi text, txtcercacorregidaod text, txtcercacorregidaoi text, txtlejoscorregidaod text, txtlejoscorregidaoi text, txtcercaagujerood text, txtcercaagujerooi text, txtlejosagujerood text, txtlejosagujerooi text, txtbinocularsincorregir text, txtbinocularcorregida text, rbtecishihara_normal boolean, rbtecishihara_anormal boolean, rbtecishihara_nc boolean, rbteccoleres_normal boolean, rbteccoleres_anormal boolean, rbteccoleres_nc boolean, txttecestereopsia text, rbtecestereopsia_normal boolean, rbtecestereopsia_anormal boolean, rbtecestereopsia_nc boolean, chkrefraccionaplica boolean, chkrefraccionnoaplica boolean, txtlejosodsf text, txtlejosodcil text, txtlejosodeje text, txtlejosoisf text, txtlejosoicil text, txtlejosoieje text, txtlejosoddip text, txtcercaodsf text, txtcercaodcil text, txtcercaodeje text, txtcercaoisf text, txtcercaoicil text, txtcercaoieje text, txtcercaoddip text, txtavconrefraccionlejosod text, txtavconrefraccionlejosoi text, txtavconrefraccioncercaod text, txtavconrefraccioncercaoi text, txtdiagnostico text, chkininguna boolean, chki2 boolean, chki3 boolean, chki4_cerca boolean, chki4_lejos boolean, chki5 boolean, chki6 boolean, chki7 boolean, chkr1 boolean, chkr2_lejos boolean, chkr2_cerca boolean, chkr3 boolean, chkr4 boolean, txtrp text, color integer, sede_descripcion text, dir_sede4 text, email_sede4 text, tel_sede4 text, cel_sede4 text, dir_sede3 text, email_sede3 text, tel_sede3 text, dir_sede2 text, email_sede2 text, tel_sede2 text, cel_sede2 text, dir_sede1 text, email_sede1 text, tel_sede1 text,
-txt_parpa_anex text,
-txt_corneas text,
-txt_otros_hallaz text,
-txt_conjuntivas text,
-txt_cristalino text,
-txt_ant_pers_imp text,
-txt_fam_imp text
-  ) AS
+  RETURNS TABLE(nombre text, apellido text, edad text, n_orden integer, dni integer, fecha_nac date, empresa text, contrata text, nom_examen text, sexo "char", cod_of integer, fecha_of date, rbecptosisod boolean, rbecptosisoi boolean, rbecestrabismood boolean, rbecestrabismooi boolean, rbecconjuntivitisod boolean, rbecconjuntivitisoi boolean, rbeccataratasod boolean, rbeccataratasoi boolean, rbecpterigionod boolean, rbecpterigionoi boolean, rbecpingueculaod boolean, rbecpingueculaoi boolean, rbecclalacionod boolean, rbecclalacionoi boolean, rbecotrosod boolean, rbecotrosoi boolean, txtechallazgos text, rbfonormalod boolean, rbfonormaloi boolean, rbfoanormalod boolean, rbfoanormaloi boolean, txtfohallazgos text, txtpiood text, txtpiooi text, txtpiona text, rbco_si boolean, rbco_no boolean, rbco_cerca boolean, rbco_lejos boolean, chkntcc boolean, chkntcl boolean, txtcercasincorregirod text, txtcercasincorregiroi text, txtlejossincorregirod text, txtlejossincorregiroi text, txtcercacorregidaod text, txtcercacorregidaoi text, txtlejoscorregidaod text, txtlejoscorregidaoi text, txtcercaagujerood text, txtcercaagujerooi text, txtlejosagujerood text, txtlejosagujerooi text, txtbinocularsincorregir text, txtbinocularcorregida text, rbtecishihara_normal boolean, rbtecishihara_anormal boolean, rbtecishihara_nc boolean, rbteccoleres_normal boolean, rbteccoleres_anormal boolean, rbteccoleres_nc boolean, txttecestereopsia text, rbtecestereopsia_normal boolean, rbtecestereopsia_anormal boolean, rbtecestereopsia_nc boolean, chkrefraccionaplica boolean, chkrefraccionnoaplica boolean, txtlejosodsf text, txtlejosodcil text, txtlejosodeje text, txtlejosoisf text, txtlejosoicil text, txtlejosoieje text, txtlejosoddip text, txtcercaodsf text, txtcercaodcil text, txtcercaodeje text, txtcercaoisf text, txtcercaoicil text, txtcercaoieje text, txtcercaoddip text, txtavconrefraccionlejosod text, txtavconrefraccionlejosoi text, txtavconrefraccioncercaod text, txtavconrefraccioncercaoi text, txtdiagnostico text, chkininguna boolean, chki2 boolean, chki3 boolean, chki4_cerca boolean, chki4_lejos boolean, chki5 boolean, chki6 boolean, chki7 boolean, chkr1 boolean, chkr2_lejos boolean, chkr2_cerca boolean, chkr3 boolean, chkr4 boolean, txtrp text, color integer, sede_descripcion text, dir_sede4 text, email_sede4 text, tel_sede4 text, cel_sede4 text, dir_sede3 text, email_sede3 text, tel_sede3 text, dir_sede2 text, email_sede2 text, tel_sede2 text, cel_sede2 text, dir_sede1 text, email_sede1 text, tel_sede1 text, txt_parpa_anex text, txt_corneas text, txt_otros_hallaz text, txt_conjuntivas text, txt_cristalino text, txt_ant_pers_imp text, txt_fam_imp text, area_o text) AS
 $BODY$
 BEGIN
   RETURN QUERY
@@ -149,7 +141,8 @@ BEGIN
     o.txt_conjuntivas,
     o.txt_cristalino,
     o.txt_ant_pers_imp,
-    o.txt_fam_imp
+    o.txt_fam_imp,
+    noo.area_o
 
   FROM datos_paciente dp
   INNER JOIN n_orden_ocupacional noo ON noo.cod_pa = dp.cod_pa
@@ -241,4 +234,191 @@ BEGIN
   WHERE noo.n_orden = p_norden;
 END;
 $BODY$
-  LANGUAGE plpgsql
+  LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION obtener_name_jasper(
+    norden_param bigint,
+    name_service_param text)
+  RETURNS text AS
+$BODY$
+DECLARE
+    resultado text;
+    name_empresa_busqueda_var text;
+    name_valor_microbiologia_var text;
+    name_valor_hepatitisa_var text;
+    valor_coproparasitologico_var boolean;
+    valor_cuantitativo_antigeno_var boolean;
+    valor_formato_marsa_var boolean;
+    
+    
+BEGIN
+   -- Obtener el nombre de la empresa de la historia clinica a registrar;
+    SELECT trim(razon_empresa) INTO name_empresa_busqueda_var from n_orden_ocupacional where n_orden=norden_param;
+
+   -- obtener el valor del txt en microbiologia
+    SELECT trim(txtkoh) INTO name_valor_microbiologia_var from microbiologia where n_orden=norden_param;
+
+   -- obtener el valor del txt en hepatitis
+    SELECT trim(txthepatitisa) INTO name_valor_hepatitisa_var from lhepatitis where n_orden=norden_param;
+
+   -- obtener el valor del boolean en coproparasitologico
+    SELECT tipo_coproparasitologico INTO valor_coproparasitologico_var from ac_coproparasitologico where n_orden=norden_param;
+
+   -- obtener los valores de los booleanos en examen inmunologico
+    SELECT cuantitativo_antigeno, formato_marsa INTO valor_cuantitativo_antigeno_var, valor_formato_marsa_var from examen_inmunologico where n_orden=norden_param;
+
+    IF name_service_param = 'con_panel10D' THEN
+        resultado := 'Consentimiento_Panel10D_Digitalizado';
+
+    ELSIF name_service_param = 'con_panel5D' THEN
+        IF name_empresa_busqueda_var = 'OBRASCÓN HUARTE LAIN S.A' THEN
+            resultado := 'Consentimiento_Panel5D_ohla_Digitalizado';
+        ELSE
+            resultado := 'Consentimiento_Panel5D_Digitalizado';
+        END IF;
+
+    ELSIF name_service_param = 'con_panel3D' THEN
+        resultado := 'Consentimiento_Panel3D_Digitalizado';
+
+    ELSIF name_service_param = 'con_panel2D' THEN
+        resultado := 'Consentimiento_Panel2D_Digitalizado';
+
+    ELSIF name_service_param = 'consent_Muestra_Sangre' THEN
+        resultado := 'Consentimiento_Muestra_Sangre_Digitalizado';
+
+    ELSIF name_service_param = 'consent_marihuana' THEN
+        resultado := 'Consentimiento_Marihuana_Digitalizado';
+
+    ELSIF name_service_param = 'consent_Boro' THEN
+        resultado := 'Consentimiento_Boro_Digitalizado';
+        
+    ELSIF name_service_param = 'analisis_bioquimicos' THEN
+        resultado := 'AnalisisBioquimicos_Digitalizado';
+
+    ELSIF name_service_param = 'lab_clinico' THEN
+        resultado := 'LaboratorioClinico_Digitalizado'; 
+
+    ELSIF name_service_param = 'hemograma_autom' THEN
+        resultado := 'Hematologia_Digitalizado'; 
+
+    ELSIF name_service_param = 'lgonadotropina' THEN
+        resultado := 'LGonadotropina_Digitalizado'; 
+
+    ELSIF name_service_param = 'panel2d' THEN
+        resultado := 'Panel2d_Digitalizado';  
+
+    ELSIF name_service_param = 'panel3d' THEN
+        resultado := 'Panel3d_Digitalizado';                                      
+
+    ELSIF name_service_param = 'toxicologia' THEN
+        IF name_empresa_busqueda_var = 'OBRASCÓN HUARTE LAIN S.A' THEN
+            resultado := 'ResultadosPanel5d_ohla_Digitalizado';
+        ELSE
+            resultado := 'ResultadosPanel5d_Digitalizado';
+        END IF;
+
+    ELSIF name_service_param = 'panel10d' THEN
+        resultado := 'Panel10d_Digitalizado';                                      
+
+    ELSIF name_service_param = 'inmunologia' THEN
+        resultado := 'InmunologiaLab_Digitalizado';     
+
+    ELSIF name_service_param = 'microbiologia' THEN
+	IF name_valor_microbiologia_var IS NOT NULL AND LENGTH(name_valor_microbiologia_var) > 0 THEN
+	resultado :=  'Microbiologia1_Digitalizado';
+	ELSE
+	resultado :=  'Microbiologia_Digitalizado';
+	END IF;      
+
+
+    ELSIF name_service_param = 'lhepatitis' THEN
+	IF name_valor_hepatitisa_var IS NOT NULL AND LENGTH(name_valor_hepatitisa_var) > 0 THEN
+	resultado :=  'LHepatitisA_Digitalizado';
+	ELSE
+	resultado :=  'LHepatitisB_Digitalizado';
+	END IF;      
+
+    ELSIF name_service_param = 'l_bioquimica' THEN
+        resultado := 'LBioquimica_Digitalizado';     
+
+    ELSIF name_service_param = 'ac_bioquimica2022' THEN
+        resultado := 'AnalisisClinicosB_Digitalizado';     
+
+    ELSIF name_service_param = 'perfil_hepatico' THEN
+        resultado := 'PerfilHepatico_Digitalizado';     
+
+    ELSIF name_service_param = 'ac_coprocultivo' THEN
+        resultado := 'coprocultivo_digitalizado';           
+
+    ELSIF name_service_param = 'ac_coproparasitologico' THEN
+        IF valor_coproparasitologico_var = true THEN
+            resultado := 'Coproparasitologico_Digitalizado';
+        ELSE
+            resultado := 'ParasitologiaSeriado_Digitalizado';
+        END IF;
+
+    ELSIF name_service_param = 'examen_inmunologico' THEN
+        IF valor_cuantitativo_antigeno_var = true THEN
+	        resultado := 'pcuantiantigeno';
+	ELSE
+            IF valor_formato_marsa_var = true THEN
+                resultado := 'pcualitativaantigenoMarsa';
+            ELSE
+                resultado := 'pcualitativaantigeno';
+            END IF;
+	END IF;
+    ELSIF name_service_param = 'audiometria_2023' THEN
+        IF name_empresa_busqueda_var LIKE '%MINERA BOROO MISQUICHILCA S.A.%' OR name_empresa_busqueda_var ILIKE '%EMPRESA DE TRANSPORTES N & V S.A.C.%'
+		OR name_empresa_busqueda_var ILIKE '%DEYFOR EIRL%' THEN
+            resultado := 'Audiometria2021-_Digitalizado_boro'; 
+        ELSE
+            resultado := 'Audiometria2021-_Digitalizado';
+        END IF;
+    ELSIF name_service_param = 'historia_oc_info' THEN
+        IF name_empresa_busqueda_var LIKE '%MINERA BOROO MISQUICHILCA S.A.%' OR name_empresa_busqueda_var ILIKE '%EMPRESA DE TRANSPORTES N & V S.A.C.%'
+		OR name_empresa_busqueda_var ILIKE '%DEYFOR EIRL%' THEN
+            resultado := 'HistoriaOcupacional_Digitalizado_boro'; 
+        ELSE
+            resultado := 'HistoriaOcupacional_Digitalizado';
+        END IF;
+    ELSIF name_service_param = 'audiometria_po' THEN
+        resultado := 'FichaAudiologica_Digitalizado';
+    ELSIF name_service_param = 'cuestionario_audiometria' THEN
+        resultado := 'CuestionarioAudiometria_Digitalizado';
+    ELSIF name_service_param = 'oftalmologia_lo' THEN
+        resultado := 'OftalmologiaLO';
+    ELSIF name_service_param = 'oftalmologia' THEN
+        resultado := 'Oftalmologia';
+    ELSIF name_service_param = 'oftalmologia_reporte' THEN
+        resultado := 'ReporteOftalmologico';
+    ELSIF name_service_param = 'oftalmologia2021' THEN
+	IF name_empresa_busqueda_var LIKE '%MINERA BOROO MISQUICHILCA S.A.%' OR name_empresa_busqueda_var ILIKE '%EMPRESA DE TRANSPORTES N & V S.A.C.%'
+		OR name_empresa_busqueda_var ILIKE '%DEYFOR EIRL%' THEN
+            resultado := 'EvaluacionOftalmologica2021_Digitalizado_boro';
+        ELSIF name_empresa_busqueda_var LIKE 'OBRASCÓN HUARTE LAIN S.A' THEN
+	    resultado := 'EvaluacionOftalmologica2021_Digitalizado_ohla';
+        ELSE
+            resultado := 'EvaluacionOftalmologica2021_Digitalizado';
+        END IF;
+    ELSIF name_service_param = 'odontograma' THEN
+        resultado := 'Odontograma_Digitalizado';
+    ELSIF name_service_param = 'odontograma_lo' THEN
+        resultado := 'Odontograma_lo_Digitalizado';
+    ELSIF name_service_param = 'radiografia_torax' THEN
+        resultado := 'RagiografiaToraxPA_Digitalizado';
+    ELSIF name_service_param = 'radiografia_fechas' THEN
+        resultado := 'ReporteFechasRadiografia_Digitalizado';
+    ELSIF name_service_param = 'radiografia' THEN
+        resultado := 'RAYOSXXXOFI_Digitalizado';
+    ELSIF name_service_param = 'oit' THEN
+	IF name_empresa_busqueda_var = 'OBRASCÓN HUARTE LAIN S.A' THEN
+		resultado := 'OIT_Digitalizado_boro';
+	ELSE
+		resultado := 'OIT_B_Digitalizado';
+	END IF;
+    END IF;
+    RETURN resultado;
+END;
+$BODY$
+  LANGUAGE plpgsql;
