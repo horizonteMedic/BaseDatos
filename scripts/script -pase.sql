ea0c6a95b7ee4a5277f16bf916e27920c9270718
cristian <cristian2.10@hotmail.com>
2025-08-28 15:59:48 -0500

    add: se agregó procedencia en ekg

--- scripts/script -pase.sql
+++ scripts/script -pase.sql
@@ -5,7 +5,7 @@ drop FUNCTION obtener_reporte_informe_electro_cardiograma(
 CREATE OR REPLACE FUNCTION obtener_reporte_informe_electro_cardiograma(
     IN p_norden integer,
     IN name_service text)
-  RETURNS TABLE(nombres text, sexo "char", dni integer, edad text, empresa text, contrata text, codigo_electrocardiograma integer, n_orden integer, fecha_informe date, mensaje_ritmo text, mensaje_pr text, mensaje_fc text, mensaje_qtc text, mensaje_qrs text, mensaje_eje text, hallazgo text, conclusion text, recomendaciones text, color integer, sede text, codigo_sede text, name_jasper text, onda_p text, s_t text, onda_t text, q_t text, fecha_nacimiento date, informe_completo boolean, presion_arterial text, descripcion_digitalizacion text, name_digitalizacion text, dni_digitalizacion integer) AS
+  RETURNS TABLE(nombres text, sexo "char", dni integer, edad text, empresa text, contrata text, codigo_electrocardiograma integer, n_orden integer, fecha_informe date, mensaje_ritmo text, mensaje_pr text, mensaje_fc text, mensaje_qtc text, mensaje_qrs text, mensaje_eje text, hallazgo text, conclusion text, recomendaciones text, color integer, sede text, codigo_sede text, name_jasper text, onda_p text, s_t text, onda_t text, q_t text, fecha_nacimiento date, informe_completo boolean, presion_arterial text, procedencia text, descripcion_digitalizacion text, name_digitalizacion text, dni_digitalizacion integer) AS
 $BODY$
 BEGIN
     -- Tabla temporal para los parametros digitalizados
@@ -50,6 +50,7 @@ BEGIN
         dp.fecha_nacimiento_pa,
         CASE WHEN ie.informe_completo IS NULL THEN TRUE ELSE ie.informe_completo END AS informe_completo,
         tr.sistolica || '/' || tr.diastolica || ' mmHg',
+        dp.lugar_nac_pa,
         td.descripcion,
         td.name_digitalizacion,
         td.dni
