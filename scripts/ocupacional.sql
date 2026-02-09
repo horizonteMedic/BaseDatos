04c3be1b40cabad8fa768e91a5f2c6fc24ff0978
cristian <cristian2.10@hotmail.com>
2026-02-09 15:46:56 -0500

    add: se agrego trigger para colocar la nomenclatura automatica

--- scripts/ocupacional.sql
+++ scripts/ocupacional.sql
@@ -1,12 +1,29 @@
-CREATE TABLE n_orden_firma(
-    id SERIAL PRIMARY KEY,
-    n_orden INTEGER NOT NULL,
-    profesional TEXT,
-    doctor_asignado TEXT,
-    doctor_extra TEXT,
-    usuario_registro TEXT,
-    fecha_registro DATE,
-    usuario_actualizacion TEXT,
-    fecha_actualizacion DATE,
-    FOREIGN KEY (n_orden) REFERENCES n_orden_ocupacional(n_orden)
-)
+
+
+CREATE OR REPLACE FUNCTION set_nomenclatura_interconsulta()
+    RETURNS TRIGGER AS $$
+DECLARE
+    v_count INT;
+BEGIN
+    -- Contar cuántos ya existen con el mismo dato en común
+    SELECT COUNT(*)
+    INTO v_count
+    FROM ficha_interconsulta
+    WHERE n_orden = NEW.n_orden;
+
+    -- Armar el valor de la columna
+    IF v_count = 0 THEN
+        NEW.nomenclatura := 'INTERCONSULTA';
+    ELSE
+        NEW.nomenclatura := 'INTERCONSULTA ' || (v_count + 1);
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_set_nomenclatura_interconsulta
+    BEFORE INSERT
+    ON ficha_interconsulta
+    FOR EACH ROW
+EXECUTE PROCEDURE set_nomenclatura_interconsulta();
